7b80bf9fafd20347abb1a27cfce04c968c6e09f4
Ilya Kurganskiy <ilya.kurganskii@gmail.com>
2020-04-09 23:03:01 +0400

    feat: add user model

---
+++ src/migration/1586458857558-CreateUser.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUser1586458857558 implements MigrationInterface {
+  name = "CreateUser1586458857558";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+      undefined
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`, undefined);
+  }
+}
