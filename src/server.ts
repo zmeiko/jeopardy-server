b28e99b71d20838e561653a024a1684b4da25b4a
Ilya Kurganskiy <ilya.kurganskii@gmail.com>
2020-04-09 22:17:31 +0400

    build: add prettier and eslint

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,19 @@
-import * as Koa from 'koa';
-import * as Router from 'koa-router';
-import {createConnection} from "typeorm";
+import * as Koa from "koa";
+import * as Router from "koa-router";
+import { createConnection } from "typeorm";
 
-createConnection().then(async connection => {
-
+createConnection()
+  .then(async () => {
     const app = new Koa();
     const router = new Router();
 
-    router.get('/*', async (ctx) => {
-        ctx.body = 'Hello World!';
+    router.get("/*", async (ctx) => {
+      ctx.body = "Hello World!";
     });
 
     app.use(router.routes());
-
     app.listen(3000);
 
     console.log("Koa application is up and running on port 3000");
-
-}).catch(error => console.log("TypeORM connection error: ", error));
+  })
+  .catch((error) => console.log("TypeORM connection error: ", error));
