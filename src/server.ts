f4da2ca063ad39c3214539fddf6f8a62dec866c4
Ilia Kurganskiy <ilya.kurganskii@gmail.com>
2020-04-15 19:35:20 +0400

    build: add koa as base server

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
-import { ApolloServer } from "apollo-server";
 import "reflect-metadata";
+import { ApolloServer } from "apollo-server-koa";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
 import * as resolvers from "./resolvers";
-
+import Koa from "koa";
 createConnection()
   .then(async () => {
     const schema = await buildSchema({
       resolvers: Object.values(resolvers),
     });
     const server = new ApolloServer({ schema });
-    await server.listen(3000);
-    console.log("Koa application is up and running on port 3000");
+    const app = new Koa();
+    app.use(server.getMiddleware());
+    app.listen(3000, () => {
+      console.log("Koa application is up and running on port 3000");
+    });
   })
   .catch((error) => console.log("TypeORM connection error: ", error));
